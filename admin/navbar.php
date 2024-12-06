a38c5f73f28245ee085c629c2086b51bafc6d78a
KURNIAAIW <kurniaimany15@gmail.com>
2024-12-06 16:06:44 +0700

    admin

---
+++ admin/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Sistem Informasi Pengaduan</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="dashboard.php">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="transaksi.php">Transaksi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="user_complain.php">Daftar Pengaduan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="user.php">Kelola User</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="profile.php">Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-danger" href="../auth/logout.php">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
